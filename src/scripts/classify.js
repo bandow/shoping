bb2033ed443214f86a5372032459ea1178630f57
bandow <sycbbb@sina.com>
2017-06-27 15:28:15 +0800

    shoping

--- src/scripts/classify.js
+++ src/scripts/classify.js
@@ -4,6 +4,11 @@ const haederClassify=document.querySelector(".haeder-classify");
 const aLi=haederClassify.getElementsByTagName("li");
 const integratingRange=document.querySelector(".integrating-range");
 const aDd=integratingRange.getElementsByTagName("dd");
+
+const pureIntegral=document.querySelector(".pure-integral");
+const aDd1=pureIntegral.getElementsByTagName("dd");
+const oneInt=document.getElementById("oneInt");
+
 const mask=document.querySelector(".mask");
 const classifyContainer=document.querySelector(".classify-container");
 const ifyLi=classifyContainer.getElementsByTagName("li");
@@ -22,6 +27,11 @@ for(var i=0; i<aLi.length;i++){
 		}else{
 			intHide();
 		}
+		if(this.index==0 && !flag){
+			intShow1();
+		}else{
+			intHide1();
+		}
 	}
 }
 //积分区间列表
@@ -33,6 +43,18 @@ for(var i=0;i<aDd.length;i++){
 		}
 		aDd[this.index].className = "active";
 		intHide();
+	}
+}
+//纯积分列表
+for(var i=0;i<aDd1.length;i++){
+	aDd1[i].index=i;
+	aDd1[i].onclick=function(){
+		for(var j=0;j<aDd1.length;j++){
+			aDd1[j].className="";
+		}
+		aDd1[this.index].className = "active";
+		oneInt.innerHTML=aDd1[this.index].firstChild.innerHTML;
+		intHide1();
 	}
 }
 //推荐列表
@@ -59,6 +81,7 @@ for(var i=0;i<ifyLi.length;i++){
 //点击覆盖层
 mask.onclick=function(){
 	 intHide();
+	 intHide1();
 }
 function intShow(){
 	integratingRange.className ="integrating-range on";
@@ -67,6 +90,16 @@ function intShow(){
 }
 function intHide(){
 	integratingRange.className ="integrating-range";
+	mask.style.display="none";
+	flag=false;
+}
+function intShow1(){
+	pureIntegral.className ="pure-integral on";
+	mask.style.display="block";
+	flag=true;
+}
+function intHide1(){
+	pureIntegral.className ="pure-integral";
 	mask.style.display="none";
 	flag=false;
 }
